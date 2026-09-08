45047028a1d1d68b6157d1d3acc0a22aabb70963
bisha <konihysen3@gmail.com>
2026-09-08 13:54:59 +0200

    fix

--- main.go
+++ main.go
@@ -10,33 +10,33 @@ import (
 )
 
 func main() {
-	supplier := flag.String("supplier", "", "supplier to scrape: eurooffice or officesolution (required)")
-	flag.Parse()
+        supplier := flag.String("supplier", "", "supplier to scrape: eurooffice or officesolution (required)")
+        flag.Parse()
 
-	if *supplier == "" {
-		fmt.Fprintln(os.Stderr, "Error: -supplier flag is required (eurooffice or officesolution)")
-		os.Exit(1)
-	}
+        if *supplier == "" {
+                fmt.Fprintln(os.Stderr, "Error: -supplier flag is required (eurooffice or officesolution)")
+                os.Exit(1)
+        }
 
-	var categoryURL, outDir string
-	workers := 5
+        var categoryURL, outDir string
+        workers := 5
 
-	switch *supplier {
-	case "officesolution":
-		categoryURL = "https://officesolution.al/en/11-inks-and-toners-for-printers-and-photocopiers"
-		outDir = "output/officesolution"
-		err := officesolution.Run(categoryURL, outDir, workers)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-			os.Exit(1)
-		}
-	default:
-		categoryURL = "https://eurooffice.al/product-category/bojra-printeri/"
-		outDir = "output/eurooffice"
-		err := eurooffice.Run(categoryURL, outDir, workers)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-			os.Exit(1)
-		}
-	}
+        switch *supplier {
+        case "officesolution":
+                categoryURL = "https://officesolution.al/en/11-inks-and-toners-for-printers-and-photocopiers"
+                outDir = "output/officesolution"
+                err := officesolution.Run(categoryURL, outDir, workers)
+                if err != nil {
+                        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+                        os.Exit(1)
+                }
+        default:
+                categoryURL = "https://eurooffice.al/product-category/bojra-printeri/"
+                outDir = "output/eurooffice"
+                err := eurooffice.Run(categoryURL, outDir, workers)
+                if err != nil {
+                        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+                        os.Exit(1)
+                }
+        }
 }
